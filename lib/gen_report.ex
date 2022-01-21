bda14d3e3eb84bfc17f372f0cef9a604db530c7f
Frank <franklaercio@gmail.com>
2022-01-21 02:11:38 -0300

    Adding sum total hours per user

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -1,6 +1,46 @@
 defmodule GenReport do
+  alias GenReport.Parser
 
-  def build do
-    # TO DO
+  # Daniele,7,29,4,2018
+
+  @people [
+    "cleiton",
+    "daniele",
+    "danilo",
+    "diego",
+    "giuliano",
+    "jakeliny",
+    "joseph",
+    "mayk",
+    "rafael",
+    "vinicius"
+  ]
+
+  def build(filename) do
+    filename
+    |> Parser.parse_file()
+    |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
+  end
+
+  defp sum_values(
+         [name, hours, _day, _month, _year],
+         %{
+           "all_hours" => all_hours
+         } = report
+       ) do
+    all_hours = Map.put(all_hours, name, all_hours[name] + hours)
+
+    %{
+      report
+      | "all_hours" => all_hours
+    }
+  end
+
+  def report_acc do
+    all_hours = Enum.into(@people, %{}, &{String.downcase(&1), 0})
+
+    %{
+      "all_hours" => all_hours
+    }
   end
 end
